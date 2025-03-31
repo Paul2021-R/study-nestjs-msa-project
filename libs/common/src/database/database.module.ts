280ab531b7e35aea4cc9dbbbf04f97a0e08780d0
Hansol Ryu <ryu.axel@gmail.com>
2025-03-31 22:17:48 +0900

    STUDY: set init - wsl nestjs project 세팅 - pnpm 설치 - common 라이브러리 설정 - database module 설정 - mongodb 연결 설정 - tsconfig 를 통한 path 축약 설정 - config, database 모듈에 대한 dir 단축 index.ts 설정 - env 설정 - Joi env 라이브러리 설정

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+    // imports: [ MongooseModule.forRoot(
+    //     'mongodb://hansol:ryuryu0314@localhost'
+    // )]
+    // 하드 코딩 할 때
+    imports: [MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+            uri: configService.get('MONGODB_URI'),
+        }),
+        inject: [ConfigService]
+    })]
+})
+export class DatabaseModule {}

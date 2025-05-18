70631e96dfa275e7d6bc245654b3febc365e6994
Hansol Ryu <ryu.axel@gmail.com>
2025-05-18 22:07:39 +0900

    refactor: restructure Dockerfiles and package.json for microservices; add UserDocument to common models

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -2,18 +2,18 @@ import { Controller, Get, Logger, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guards';
 import { CurrentUser } from '../../../libs/common/src/decorator/current-user.decorator';
-import { UserDocument } from './users/models/user.schema';
+
 import { Response } from 'express';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { inspect } from 'util';
 import { UsersService } from './users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserDocument } from '@app/common';
 
 @Controller('auth')
 export class AuthController {
   private readonly logger = new Logger(JwtAuthGuard.name);
-  
+
   constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
--- memo/lecture.memo.md
+++ memo/lecture.memo.md
@@ -7,3 +7,7 @@
 - 연결하고 나면 명령어를 통해 gcloud 프로젝트와 연결을 수행한다
 - 로그인 절차를 처리하고난 후, 빌드를 하고, 해당 빌드 이미지에 대해 레포지터리 uri 를 받아서, 이름을 추가하여 푸시 하면 이미지가 올라가게 된다. 
 -
+
+## 25. 프로덕션화 , Dockerfile refactoring
+- 빌드를 각 Dockerfile 별로 할 경우 UserDocument 가 걸림(auth 영역). 해당 영역은 auth 영역 내부에 있으므로 common 에 추가하여 변경 후 빌드한다.  
+- 이때 Package.json 의 의존성에서 각 마이크로 서버 별로 필요한 의존성이 다를 수 있고, 이럴 때는 내부 하위에 package.json 을 만들고, 전체 중 필요한 영역을 제외한 공통 package.json / 개별 영역의 package.json 을 만들어 활용하면 된다 
